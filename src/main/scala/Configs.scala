49508ae3389f4cab30844e8779b3800a8f3e5a38
Jiajie Chen <noc@jiegec.ac.cn>
2019-11-04 22:56:21 +0800

    Update mem/mmio id bits

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -1,16 +1,22 @@
 package thinpad
 
 import chisel3._
-import freechips.rocketchip.config.{Parameters, Config}
-import freechips.rocketchip.subsystem._
+import freechips.rocketchip.config._
 import freechips.rocketchip.devices.debug._
 import freechips.rocketchip.devices.tilelink.BootROMParams
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.subsystem.MemoryPortParams
 import freechips.rocketchip.rocket.{RocketCoreParams, MulDivParams, DCacheParams, ICacheParams}
 import freechips.rocketchip.tile.{RocketTileParams, XLen}
 
 class WithBootROM extends Config((site, here, up) => {
   case BootROMParams => BootROMParams(
     contentFileName = s"./bootrom/bootrom.rv${site(XLen)}.img")
+})
+
+class WithIDBits extends Config((site, here, up) => {
+  case ExtMem => up(ExtMem, site).map(x => x.copy(master = x.master.copy(idBits = 5)))
+  case ExtBus => up(ExtBus, site).map(x => x.copy(idBits = 5))
 })
 
 class WithNMediumCores(n: Int) extends Config((site, here, up) => {
@@ -36,12 +42,12 @@ class WithNMediumCores(n: Int) extends Config((site, here, up) => {
 })
 
 class DefaultConfig extends Config(
-  new WithBootROM ++ new freechips.rocketchip.system.DefaultConfig)
+  new WithIDBits ++ new WithBootROM ++ new freechips.rocketchip.system.DefaultConfig)
 class DefaultMediumConfig extends Config(
-  new WithBootROM ++ new WithNMediumCores(1) ++
+  new WithIDBits ++ new WithBootROM ++ new WithNMediumCores(1) ++
   new freechips.rocketchip.system.BaseConfig)
 class DefaultSmallConfig extends Config(
-  new WithBootROM ++ new freechips.rocketchip.system.DefaultSmallConfig)
+  new WithIDBits ++ new WithBootROM ++ new freechips.rocketchip.system.DefaultSmallConfig)
 
 class ThinpadFPGAConfig extends Config(new WithoutTLMonitors ++ new DefaultConfig)
 class ThinpadMediumFPGAConfig extends Config(new WithoutTLMonitors ++ new DefaultMediumConfig)
